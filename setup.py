c78b03bab707d000e067215204dc3c8e604dee42
jaydesl <jay.deslauriers@gmail.com>
2022-01-04 16:55:14 +0000

    chore: create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="micado-parser",
+    description="Parse MiCADO ADTs for the MiCADO Submitter",
+    version="0.8.0",
+    author="Jay DesLauriers",
+    packages=find_packages(exclude=['tests']),
+    install_requires=["ruamel.yaml", "tosca-parser"],
+
+    python_requires=">=3.8",
+    # entry_points={
+    #     "console_scripts": ["micadoparser=micadoparser.cli:main"],
+    # },
+)
